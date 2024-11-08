2b49b236b7d6130113afedae9ba6ebdc9f5ab886
Divyanshu Prajapati <ankurprajapati2023@gmail.com>
2024-11-09 03:38:50 +0530

    Initail-Commit

---
+++ Schema.sql
@@ -0,0 +1,11 @@
+create database MeditationPlatform;
+use MeditationPlatform;
+create table users
+(
+id integer auto_increment primary key,
+uname varchar(100) not null,
+email varchar(50) not null unique,
+country varchar(50) not null,
+passwd varchar(20) not null
+
+);
